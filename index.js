ff0907236f13e8aeee5b37067d24d836cb589f33
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2024-02-24 10:34:46 +0100

    added x to incomplete side notes and fixed padding

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pathofexile.com Challenges
 // @namespace    http://tampermonkey.net/
-// @version      000.005.0015
+// @version      000.005.0016
 // @updateURL    https://raw.githubusercontent.com/danogo2/pathofexile.com-challenges/main/index.js
 // @downloadURL  https://raw.githubusercontent.com/danogo2/pathofexile.com-challenges/main/index.js
 // @description  path of exile challenges extension
@@ -320,8 +320,8 @@ option.tag-custom {
   gap: 10px;
 }
 
-.note-completion-check.incomplete {
-  visibility: hidden;
+.note-completion-check {
+  width: 10px;
 }
 
 /* hide toggle button */
@@ -637,6 +637,12 @@ option.tag-custom {
 
   const svgIconMinimize =
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M64 80c-8.8 0-16 7.2-16 16V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V96c0-8.8-7.2-16-16-16H64zM0 96C0 60.7 28.7 32 64 32H384c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM152 232H296c13.3 0 24 10.7 24 24s-10.7 24-24 24H152c-13.3 0-24-10.7-24-24s10.7-24 24-24z" fill="#beb698"/></svg>';
+
+  const svgIconImport =
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#beb698" d="M128 64c0-35.3 28.7-64 64-64H352V128c0 17.7 14.3 32 32 32H512V448c0 35.3-28.7 64-64 64H192c-35.3 0-64-28.7-64-64V336H302.1l-39 39c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l80-80c9.4-9.4 9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l39 39H128V64zm0 224v48H24c-13.3 0-24-10.7-24-24s10.7-24 24-24H128zM512 128H384V0L512 128z"/></svg>';
+
+  const svgIconExport =
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#beb698" d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V288H216c-13.3 0-24 10.7-24 24s10.7 24 24 24H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zM384 336V288H494.1l-39-39c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l80 80c9.4 9.4 9.4 24.6 0 33.9l-80 80c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l39-39H384zm0-208H256V0L384 128z"/></svg>';
 
   // decodeURIComponent is necessary for leagues separated by /
   const state = {
@@ -1020,9 +1026,9 @@ option.tag-custom {
         'beforeend',
         `<li class="side-challenge ${note.length ? '' : 'hidden'} ${
           isComplete ? 'complete' : 'incomplete'
-        }" data-id="${id}"><h3 class="side-challenge-header" title="scroll to challenge" data-id="${id}"><span class="side-challenge-header--text">${name}</span><div class="goto-container"><div class="icon-goto">${svgIconArrowGoTo}</div></div><div class="note-completion"><span class="note-progress">${completionProgress}</span><span class="note-completion-check ${
-          isComplete ? '' : 'incomplete'
-        }">✓</span></div></h3><div class="side-note" data-id="${id}">${note}</div></li>`
+        }" data-id="${id}"><h3 class="side-challenge-header" title="scroll to challenge" data-id="${id}"><span class="side-challenge-header--text">${name}</span><div class="goto-container"><div class="icon-goto">${svgIconArrowGoTo}</div></div><div class="note-completion"><span class="note-progress">${completionProgress}</span><span class="note-completion-check">${
+          isComplete ? '✓' : '⨉'
+        }</span></div></h3><div class="side-note" data-id="${id}">${note}</div></li>`
       );
     }
     document
