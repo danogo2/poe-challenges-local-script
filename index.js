4d0e902405783a3074b1b0b7e22f9bf73fc18976
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2024-02-25 02:23:37 +0100

    added option for importing external file with tags and notes for all leagues

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pathofexile.com Challenges
 // @namespace    http://tampermonkey.net/
-// @version      000.006.000
+// @version      000.007.000
 // @updateURL    https://raw.githubusercontent.com/danogo2/pathofexile.com-challenges/main/index.js
 // @downloadURL  https://raw.githubusercontent.com/danogo2/pathofexile.com-challenges/main/index.js
 // @description  path of exile challenges extension
@@ -354,6 +354,11 @@ option.tag-custom {
   width: 20px;
 }
 
+.icon-import svg,
+.icont-export svg {
+  height: 21px;
+}
+
 /* challenge box */
 .profile
   .profile-container
@@ -613,7 +618,6 @@ option.tag-custom {
 .hide-completed .achievement:not(.incomplete) {
   display: none;
 }
-
   `);
 
   const svgIconEye =
@@ -863,6 +867,14 @@ option.tag-custom {
       `<div class="settings-option"><button class='button-settings button-export'><div class="settings-icon icon-export" title="export tags and notes">${svgIconExport}</div></button></div>`
     );
     return parentEl.querySelector('.button-export');
+  };
+
+  const insertImportButtonEl = parentEl => {
+    parentEl.insertAdjacentHTML(
+      'beforeend',
+      `<div class="settings-option"><button class='button-settings button-import'><div class="settings-icon icon-import" title="import tags and notes">${svgIconImport}</div></button></div>`
+    );
+    return parentEl.querySelector('.button-import');
   };
 
   const insertTagSelectEl = parentEl => {
@@ -1198,6 +1210,7 @@ option.tag-custom {
     settingsEl.insertAdjacentElement('beforeend', leagueSelectEl);
     const hideButtonEl = insertHideButtonEl(settingsEl);
     const clearButtonEl = insertClearButtonEl(settingsEl);
+    const importButtonEl = insertImportButtonEl(settingsEl);
     const exportButtonEl = insertExportButtonEl(settingsEl);
     titleEl.textContent = titleEl.textContent.replace('Challenges', '').trim();
     settingsEl.insertAdjacentElement('beforeend', titleEl);
@@ -1211,6 +1224,26 @@ option.tag-custom {
       'click',
       async event => await clickExportButtonHandler(event)
     );
+    importButtonEl.addEventListener('click', async event => {
+      const file = await openFileOrFiles();
+      if (!file) {
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.addEventListener('load', event => {
+        const fileContent = event.target.result;
+        const importedStateJSON = atob(fileContent);
+        const shouldLoadImportedState = confirm(
+          'Load external tags and notes for all leagues?'
+        );
+        if (shouldLoadImportedState) {
+          localStorage.setItem('poe', importedStateJSON);
+          location.reload();
+        }
+      });
+      reader.readAsText(file);
+    });
   };
 
   const changeChallStyle = (id, challEl) => {
@@ -1566,37 +1599,6 @@ option.tag-custom {
   };
 
   const saveFile = async (blob, suggestedName) => {
-    // Feature detection. The API needs to be supported
-    // and the app not run in an iframe.
-    // const supportsFileSystemAccess =
-    //   'showSaveFilePicker' in window &&
-    //   (() => {
-    //     try {
-    //       return window.self === window.top;
-    //     } catch {
-    //       return false;
-    //     }
-    //   })();
-    // // If the File System Access API is supported…
-    // if (supportsFileSystemAccess) {
-    //   try {
-    //     // Show the file save dialog.
-    //     const handle = await window.showSaveFilePicker({
-    //       suggestedName,
-    //     });
-    //     // Write the blob to the file.
-    //     const writable = await handle.createWritable();
-    //     await writable.write(blob);
-    //     await writable.close();
-    //     return;
-    //   } catch (err) {
-    //     // Fail silently if the user has simply canceled the dialog.
-    //     if (err.name !== 'AbortError') {
-    //       console.error(err.name, err.message);
-    //       return;
-    //     }
-    //   }
-    // }
     // Fallback if the File System Access API is not supported…
     // Create the blob URL.
     const blobURL = URL.createObjectURL(blob);
@@ -1613,6 +1615,37 @@ option.tag-custom {
       URL.revokeObjectURL(blobURL);
       a.remove();
     }, 1000);
+  };
+
+  const openFileOrFiles = async (multiple = false) => {
+    // Fallback if the File System Access API is not supported.
+    return new Promise(resolve => {
+      // Append a new `<input type="file" multiple? />` and hide it.
+      const input = document.createElement('input');
+      input.style.display = 'none';
+      input.type = 'file';
+      document.body.append(input);
+      if (multiple) {
+        input.multiple = true;
+      }
+      // The `change` event fires when the user interacts with the dialog.
+      input.addEventListener('change', () => {
+        // Remove the `<input type="file" multiple? />` again from the DOM.
+        input.remove();
+        // If no files were selected, return.
+        if (!input.files) {
+          return;
+        }
+        // Return all files or just one file.
+        resolve(multiple ? Array.from(input.files) : input.files[0]);
+      });
+      // Show the picker.
+      if ('showPicker' in HTMLInputElement.prototype) {
+        input.showPicker();
+      } else {
+        input.click();
+      }
+    });
   };
 
   const clickExportButtonHandler = async event => {
